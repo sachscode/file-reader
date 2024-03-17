7eae10b3dec1092733f77682d02076b75dbcb57b
Sachin Panemangalore <sachscode@gmail.com>
2024-03-18 01:28:03 +0530

    file-reader from args pending ut

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Please provide a file name as an argument");
+    }
+    let file_name = &args[1];
+    let file = File::open(file_name);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
